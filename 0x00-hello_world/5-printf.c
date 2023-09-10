4b27b4e2142421d1d1736d2224df4b8c4d384198
wafula wanyonyi <wanyonyiwafuladavis@gmail.com>
2023-09-09 21:20:09 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+/**
+ * main - main function of the project
+ * Return: is always 0 when successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("\n");
+	return (0);
+
+}
