5ea81583304230ae452b90575c163f2e90d01a4e
wafula wanyonyi <wanyonyiwafuladavis@gmail.com>
2023-09-10 21:31:14 -0700

    100-intel

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+# include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always return 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+	int m;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+
+	return (0);
+}
