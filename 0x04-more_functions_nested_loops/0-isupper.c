1935a7a8ca89b53ec93dd0186f316e65dff15760
wafula wanyonyi <wanyonyiwafuladavis@gmail.com>
2023-09-14 17:25:08 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+# include "main.h"
+/**
+ * _isupper - create uppercase
+ * @c: function parameter
+ * Return: always 0 on failure and 1 on success
+ */
+int _isupper(int c)
+
+	if (c >= 'A' && c <= 'Z')
+	return (1);
+
+	else
+	return (0);
