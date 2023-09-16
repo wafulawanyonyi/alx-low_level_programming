95bda7cb4fc0b697dfd64af8802b10d8a7c0af28
wafula wanyonyi <wanyonyiwafuladavis@gmail.com>
2023-09-16 12:11:44 -0700

    ./8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ * prints all numbers of base 16 in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char hex_digit;
+
+	for (hex_digit = '0'; hex_digit <= '9'; hex_digit++)
+	{
+		putchar(hex_digit);
+	}
+
+	for (hex_digit = 'a'; hex_digit <= 'f'; hex_digit++)
+	{
+		putchar(hex_digit);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
