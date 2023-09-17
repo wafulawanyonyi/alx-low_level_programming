a1594e33305a7821fad457fed76ce1ecfb6ed44a
wafula wanyonyi <wanyonyiwafuladavis@gmail.com>
2023-09-17 02:20:37 -0700

    ./3-islower

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,4 +4,8 @@ int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 void print_times_table(int n);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
 #endif
