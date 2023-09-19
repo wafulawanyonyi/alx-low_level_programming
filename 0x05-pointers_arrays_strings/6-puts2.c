7e9fc01532567cd7ee836475bfdc607ed9d1ffe6
wafula wanyonyi <wanyonyiwafuladavis@gmail.com>
2023-09-19 12:28:54 -0700

    string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string.
+ * @str: Pointer to the string.
+ */
+void puts2(char *str)
+{
+	if (str != NULL)
+	{
+		int i = 0;
+
+		while (str[i] != '\0')
+		{
+			_putchar(str[i]);
+			i += 2;
+		}
+
+		_putchar('\n');
+	}
+}
+
