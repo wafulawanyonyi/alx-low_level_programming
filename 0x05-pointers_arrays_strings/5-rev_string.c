197d9422a64e3dd8af4fbc959bff08a0ea44adfb
wafula wanyonyi <wanyonyiwafuladavis@gmail.com>
2023-09-19 12:17:05 -0700

    reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,42 +1,33 @@
-/* reverse_string.c */
-
 #include "main.h"
 
 /**
- * rev_string - Reverses a string
- * @s: The input string
- *
- * This function reverses a string in-place.
- *
- * @s: Pointer to the input string to be reversed.
+ * rev_string - Reverses a string.
+ * @s: Pointer to the string to be reversed.
  */
 void rev_string(char *s)
 {
-    char *end, temp;
-    int length = 0;
+	if (s != NULL)
+	{
+		int length = 0;
+		char *start = s;
+		char *end = s;
 
-    if (s == NULL)
-        return;
+		while (*end != '\0')
+		{
+			length++;
+			end++;
+		}
 
-    end = s;
+		end--;
 
-    // Calculate the length of the string
-    while (*end != '\0')
-    {
-        length++;
-        end++;
-    }
-
-    end--; // Point to the last character before the null terminator
-
-    // Swap characters from the beginning and end of the string
-    while (s < end)
-    {
-        temp = *s;
-        *s = *end;
-        *end = temp;
-        s++;
-        end--;
-    }
+		while (start < end)
+		{
+			char temp = *start;
+			*start = *end;
+			*end = temp;
+			start++;
+			end--;
+		}
+	}
 }
 
