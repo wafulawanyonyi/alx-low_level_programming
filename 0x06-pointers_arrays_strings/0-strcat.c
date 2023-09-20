8eb8020838dacc4eb308345b4e51c46786fe9733
wafula wanyonyi <wanyonyiwafuladavis@gmail.com>
2023-09-20 12:05:30 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: destination string.
+ * @src: The source string to append to dest.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ptr = dest;
+
+	while (*ptr != '\0')
+	{
+	ptr++;
+	}
+
+	while (*src != '\0')
+	{
+	*ptr = *src;
+	ptr++;
+	src++;
+	}
+
+	*ptr = '\0';
+
+	return (dest);
+}
