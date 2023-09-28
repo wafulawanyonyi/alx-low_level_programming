756f506ec40311c511800af04fa9a6c64076f857
wafula wanyonyi <wanyonyiwafuladavis@gmail.com>
2023-09-27 18:17:13 -0700

    ./2-strlen_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Calculates the length of a string.
+ * @s: Pointer to the string for which length is to be calculated.
+ * Return: Length of the string.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+
+	return (1 + _strlen_recursion(s + 1));
+}
+
