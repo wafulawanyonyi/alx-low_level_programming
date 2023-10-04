50162887f4a484f09aade62994affc0ff855eae5
wafula wanyonyi <wanyonyiwafuladavis@gmail.com>
2023-10-04 08:42:45 -0700

    ./nargs

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+	printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
