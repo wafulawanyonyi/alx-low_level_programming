c7f9cdfb56fb834fdadf7a8532ee83aa836c38c5
wafula wanyonyi <wanyonyiwafuladavis@gmail.com>
2023-10-04 08:24:14 -0700

    ./mynameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments (unused)
+ * @argv: An array containing the command-line arguments
+ *
+ * Return: Always 0
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
